1ec5498e23bcb09c38117d926dfe6a6d25ef37d1
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-07-03 17:01:31 +0530

    userModel

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  env: {
+    BASE_URL: "http://localhost:3000",
+    MONGODB_URL:
+      "mongodb+srv://admin:admin@cluster0.gq16v.mongodb.net/next-mern-ecom?retryWrites=true&w=majority",
+  },
+};
